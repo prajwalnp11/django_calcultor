d866c08508f2f2d0117391523f7499081b76e5c6
prajwalnp11 <prajwalnp9999@gmail.com>
2026-07-10 21:48:50 +0530

    Set calculatorapp as the homepage

--- djangocalculator/urls.py
+++ djangocalculator/urls.py
@@ -20,7 +20,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("",include("rootapp.urls")),
-    
+    path("", include("calculatorapp.urls")),
     path("calculatorapp/", include("calculatorapp.urls"))
 ]
